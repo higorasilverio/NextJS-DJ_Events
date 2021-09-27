3aafcc2d4f298cc2f3abfcf863c7851400ca941e
higorasilverio <higorasilverio@outlook.com>
2021-09-27 16:18:10 -0300

    ✨ src: initial configuration

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Layout from "@/components/layout";
+
+export default function About() {
+  return (
+    <Layout title="About DJ Events">
+      <h1>About</h1>
+      <p>This is a App to show the DJ Events</p>
+      <p>Version 1.0.0</p>
+    </Layout>
+  );
+}
